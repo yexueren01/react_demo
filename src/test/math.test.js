5327f9e5a613f9b231f68fe551caecb2908dbab3
ke.zhang <ke.zhang@alipay.com>
2017-09-01 11:13:52 +0800

    reducer

--- src/test/math.test.js
+++ src/test/math.test.js
@@ -1,16 +1,24 @@
 
-// function math(state = 0, action)   {
-//     switch (action.type) {
-//         case 'INCREMENT':
-//             return state + 1
-//         case 'DECREMENT':
-//             return state - 1
-//         default:
-//             return state
-//     }
-// }
+'use strict';
+// let add = require('./add.js');
+const expect = require('chai').expect;
+const assert = require('assert');
 
-// it('call reducers math', function () {
-//     expect(math(1,{type:'INCREMENT'})).to.be.equal(2);
-//     expect(math(1,{type:'DECREMENT'})).to.be.equal(0);
-// });
+function math(state = 0, action)   {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        default:
+            return state
+    }
+}
+
+describe('math', function() {
+    it("start", function () {
+        expect(math(1,{type:'INCREMENT'})).to.be.equal(2);
+        expect(math(2,{type:'INCREMENT'})).to.be.equal(3);
+        expect(math(1,{type:'DECREMENT'})).to.be.equal(0);
+    });
+  });
